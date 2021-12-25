263310b51a8d3fad70e7d271a3bc6c520be10879
venus <528723568@qq.com>
2021-12-25 10:06:46 +0800

    idea的第一次提交

---
+++ src/main/java/com/venus/GitClass.java
@@ -0,0 +1,12 @@
+package com.venus;
+
+/**
+ * @author jinx
+ * @create 2021-12-25 10:04
+ * @description
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
+}
