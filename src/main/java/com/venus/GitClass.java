f3c36250233a1ccb4ea547349e2707f93df357e4
venus <528723568@qq.com>
2021-12-25 10:13:06 +0800

    hot-fix的分支冲突测试1

--- src/main/java/com/venus/GitClass.java
+++ src/main/java/com/venus/GitClass.java
@@ -9,5 +9,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("hot-fix1");
+        System.out.println("hot-fix分支冲突测试1");
     }
 }
